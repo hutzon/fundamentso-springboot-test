a6e4eae83e2f5ee56bccac10aa9fb1b818473266
Ottoniel Campos <hutzon4011@gmail.com>
2022-05-21 10:02:32 -0600

    first commit

---
+++ src/test/java/com/fundamentosspringboottest/fundamentostest/FundamentostestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosspringboottest.fundamentostest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentostestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
